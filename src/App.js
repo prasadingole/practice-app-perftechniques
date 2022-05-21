e6fd41eb86ed2b8608f8f35eb08bd3dad84af37d
Prasad Ingole <10679929+prasadingole@users.noreply.github.com>
2022-05-21 22:17:50 +1000

    added extra button to check how to handle closures created bu useCallback

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ import DemoOutput from './components/DemoOutput/DemoOutput';
 function App() {
   console.log("App Running");
   const [showParagraph, setShowParagraph] = useState(false);
+  const [toggleParagraphButton, setToggleParagraphButton] = useState(false);
   const toggleParagraphHandler = useCallback(() => {
+    if(toggleParagraphButton){
     setShowParagraph((previousShowParagrpah)=> !previousShowParagrpah);
-  },[]);
+    }
+  },[toggleParagraphButton]);
+
+  const toggleParagraphButtonHandler = () => {
+    setToggleParagraphButton((toggleParagraphButton)=> {
+      return !toggleParagraphButton;
+    })
+  }
   
   return (
     <div className="app">
       <h1>Hi there!</h1>
        <DemoOutput show={showParagraph}/>
-      <Button onClick={toggleParagraphHandler}>Show Paragraph !</Button>
+       <Button onClick={toggleParagraphButtonHandler}>Toggle Show Paragraph Button Effect !</Button>
+       <Button onClick={toggleParagraphHandler}>Show Paragraph !</Button>
     </div>
   );
 }
